9d16a47c3b3822fcae211de73b65016a0f20cb2e
Manish <jon.manish.adh@gmail.com>
2018-11-26 16:01:13 +0545

    Added flat components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
+import Marker from './components/marker';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      this.setState({
+        flats: data
+      })
+    });
+  }
+  
   render() {
+    const center = {
+      lat: 48.002,
+      lng: 2.1,
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat}/>
+            })}
+          </div>
+        </div>
+        <div className="map">
+        <GoogleMapReact
+          center={center}
+          zoom={1}>
+
+          {this.state.flats.map((flat) => {
+              return <Marker flat={flat}/>
+            })}
+        </GoogleMapReact>
+        </div>
       </div>
     );
   }
